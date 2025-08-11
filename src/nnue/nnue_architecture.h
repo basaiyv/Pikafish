dbbbdc84e6168562d63720438fe9361e87f052be
basaiyv <115393375+basaiyv@users.noreply.github.com>
2025-08-11 14:33:46 +0800

    Update nnue_architecture.h

--- src/nnue/nnue_architecture.h
+++ src/nnue/nnue_architecture.h
@@ -48,7 +48,7 @@ constexpr IndexType LayerStacks = 16;
 // If vector instructions are enabled, we update and refresh the
 // accumulator tile by tile such that each tile fits in the CPU's
 // vector registers.
-static_assert(PSQTBuckets % 8 == 0,
+static_assert(PSQTBuckets % 16 == 0,
               "Per feature PSQT values cannot be processed at granularity lower than 8 at a time.");
 
 template<IndexType L1, int L2, int L3>
