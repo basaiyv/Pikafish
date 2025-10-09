ac57e958407d2de1332c26cebc124e05701e2343
basaiyv <115393375+basaiyv@users.noreply.github.com>
2025-10-09 12:39:13 +0800

    Update position.cpp

--- src/position.cpp
+++ src/position.cpp
@@ -948,9 +948,7 @@ Value Position::detect_chases(int d, int ply) {
     uint16_t chase[COLOR_NB] = {0xFFFF, 0xFFFF};
     for (int i = 0; i < d; ++i)
     {
-        if (st->checkersBB)
-            return VALUE_DRAW;
-        else if (!chase[~sideToMove])
+      if (!chase[~sideToMove])
         {
             if (!chase[sideToMove])
                 break;
@@ -1018,7 +1016,7 @@ bool Position::rule_judge(Value& result, int ply) {
                 if (filter[st->key] <= 1)
                 {
                     // Not exceeding rule 60 and have the same previous step
-                    if (st->rule60 < 120 && st->previous->key == stp->previous->key)
+                    if (st->rule60 < 160 && st->previous->key == stp->previous->key)
                     {
                         // Even if we entering this loop again, it will not lead to a 3 fold repetition
                         StateInfo* prev = st->previous;
@@ -1039,7 +1037,7 @@ bool Position::rule_judge(Value& result, int ply) {
     }
 
     // 60 move rule
-    if (st->rule60 >= 120)
+    if (st->rule60 >= 160)
     {
         result = MoveList<LEGAL>(*this).size() ? VALUE_DRAW : mated_in(ply);
         return true;
